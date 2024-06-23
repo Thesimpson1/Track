0baaef4a20ab7f0b89e7cf5157efb1fe409cdcf3
ivanhassay <ivan@>
2024-06-24 00:26:39 +0200

    feat: added custom button

---
+++ app/screens/Home/components/BottomModal/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import {colors, MainColorName} from '../../../../../../constants/color.ts';
+import {
+  calcFontSize,
+  calcHeight,
+  calcWidth,
+} from '../../../../../../utils/scaling-system';
+interface CustomButtonI {
+  value: string;
+  onPress: () => void;
+  disable: boolean;
+}
+export const CustomButton = ({value, onPress, disable}: CustomButtonI) => {
+  const wrapperStyle = disable ? styles.wrapperDisable : styles.wrapperActive;
+  const textStyle = disable ? styles.textDisable : styles.textActive;
+  return (
+    <TouchableOpacity
+      style={[styles.wrapper, wrapperStyle]}
+      onPress={onPress}
+      disabled={disable}>
+      <Text style={[styles.text, textStyle]}>{value}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: calcHeight(56),
+    width: calcWidth(335),
+
+    alignSelf: 'center',
+    borderRadius: calcWidth(16),
+    justifyContent: 'center',
+    marginVertical: calcHeight(20),
+  },
+
+  text: {
+    fontSize: calcFontSize(16),
+    fontFamily: 'Lazzer-SemiBold',
+    textAlign: 'center',
+  },
+  textActive: {
+    color: colors[MainColorName.BLACK],
+  },
+  textDisable: {
+    color: colors[MainColorName.WHITE],
+  },
+  wrapperActive: {
+    backgroundColor: colors[MainColorName.GREEN],
+  },
+  wrapperDisable: {
+    backgroundColor: colors[MainColorName.DARK_GREY],
+    opacity: 0.48,
+  },
+});
